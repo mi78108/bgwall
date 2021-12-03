fbadf2d8ffeb66c4d2615075b0466439d782598e
mi78108 <mi78108@live.com>
2021-12-03 16:31:17 +0800

    添加命令参数 -pd/tmp/bgwall -ps/home/user/.config/bgwall/post_script -fs/home/user/.config/bgwall/fetch_script -du30

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #[macro_use]
 use lazy_static;
 use std::{time::{Duration, SystemTime, UNIX_EPOCH}, thread, process::{Command, ExitStatus}, ops::Add, fs::{ReadDir, DirEntry}, os::unix::prelude::MetadataExt, path::{Path, PathBuf}, fmt::Error, collections::HashMap, cell::RefCell, sync::{Arc, RwLock}, rc::Rc, borrow::{BorrowMut, Borrow}};
+use std::env::Args;
+use std::panic::panic_any;
+use std::process::exit;
 
 
 fn download_image() -> String {
     let mut _d_img = IMG_DIR.to_string().add("/");
-    if let Some(scripts) = Path::new(*FETCH_SCRIPT_DIR).get_exec_script() {
+    if let Some(scripts) = Path::new(&(*FETCH_SCRIPT_DIR)).get_exec_script() {
         let index = (|| {
             if let Ok(read) = VALS.read() {
                 return read.get("FETCH_INDEX").unwrap().parse::<usize>().unwrap();
@@ -31,30 +34,33 @@ fn download_image() -> String {
                 return (0, 0, None);
             })();
             _d_img.push_str(script.file_name().to_string_lossy().add("_").add(val.1.to_string().as_str()).borrow());
-            format!("执行下载脚本 [{:?}] [{}] [{}]",script.file_name(),_d_img,val.0).as_str().to_log();
+            format!("执行下载脚本 [{:?}] [{}] [{}]",script.path(),_d_img,val.0).as_str().to_log();
             if let Ok(mut child) = Command::new(script.path())
                 .arg(_d_img.as_str())
                 .arg(val.0.to_string()).spawn()
             {
                 if let Ok(code) = child.wait() {
                     if code.success() {
-                        format!("执行处理脚本 [{:?}] [{}] [{}] 成功; 脚本正常退出",script.file_name(),_d_img,val.0).as_str().to_log();
+                        format!("执行下载脚本 [{:?}] [{}] [{}] 成功; 脚本正常退出",script.file_name(),_d_img,val.0).as_str().to_log();
                         return _d_img;
                     }
                 }
             }
             format!("执行下载脚本 [{:?}] [{}] [{}] 失败; 脚本异常退出",script.file_name(),_d_img,val.0).as_str().to_log();
         }
+    }else {
+        format!("下载脚本目录不存在; 退出 [{}]",&(*FETCH_SCRIPT_DIR)).as_str().to_log();
+        exit(128)
     }
     return _d_img;
 }
 
 fn process_image() -> String {
     let mut _c_img = IMG_DIR.to_string().add("/").add(C_BG);
-    if let Some(scripts) = Path::new(*POST_SCRIPT_DIR).get_exec_script() {
+    if let Some(scripts) = Path::new(&(*POST_SCRIPT_DIR)).get_exec_script() {
         scripts.iter().enumerate().for_each(|(index, fd)| {
             let _n_img = IMG_DIR.to_string().add("/").add(index.to_string().as_str()).add("_").add(Path::new(_c_img.as_str()).file_name().unwrap().to_str().unwrap());
-            format!("执行处理脚本 [{:?}] [{}] [{}]",fd.file_name(),_c_img,_n_img).as_str().to_log();
+            format!("执行处理脚本 [{:?}] [{}] [{}]",fd.path(),_c_img,_n_img).as_str().to_log();
             if let Ok(mut child) = Command::new(fd.path())
                 .arg(_c_img.as_str())
                 .arg(_n_img.as_str()).spawn()
@@ -93,7 +99,7 @@ fn set_background(img_path: String) {
 
 fn change_background() {
     set_background(IMG_DIR.to_string().add("/").add(N_BG));
-    // process next image 
+    // process next image
     thread::spawn(|| {
         let post_img = process_image();
         std::fs::copy(post_img.as_str(), IMG_DIR.to_string().add("/").add(N_BG)).expect("change image -> std::fs::copy erro");
@@ -105,10 +111,35 @@ static C_BG: &str = "bgwall_next";
 static N_BG: &str = "bgwall_current";
 
 lazy_static::lazy_static! {
-    static ref IMG_DIR:&'static str = "/tmp/hawk";
-    static ref FETCH_SCRIPT_DIR:&'static str = "/home/hawk/Sync/bin/bgwall/fetch_script";
-    static ref POST_SCRIPT_DIR:&'static str = "/home/hawk/Sync/bin/bgwall/post_script";
-    static ref DURA:u64 = 5 * 60;
+    static ref IMG_DIR:String = {
+        if let Some(val)= std::env::args().get_arg_name("-pd"){
+            format!("图片存放目录设置为 [{}]",&val).as_str().to_log();
+            return  val
+        }
+        return "/tmp/bgwall".to_string();
+    };
+    static ref FETCH_SCRIPT_DIR:String = {
+        if let Some(val)= std::env::args().get_arg_name("-fs"){
+            format!("下载脚本目录设置为 [{}]",&val).as_str().to_log();
+            return  val
+        }
+        return std::env::var("HOME").unwrap_or("./".to_string()).add(".config/bgwall/fetch_script")
+    };
+    static ref POST_SCRIPT_DIR:String = {
+        //"/home/hawk/Sync/bin/bgwall/post_script";
+        if let Some(val)= std::env::args().get_arg_name("-ps"){
+            format!("处理脚本目录设置为 [{}]",&val).as_str().to_log();
+            return  val
+        }
+        return std::env::var("HOME").unwrap_or("./".to_string()).add(".config/bgwall/post_script")
+    };
+    static ref DURA:u64 =  {
+        if let Some(val)= std::env::args().get_arg_name("-du"){
+            format!("壁纸轮换间隔设置为 [{}]",&val).as_str().to_log();
+            return  60 * val.parse::<u64>().unwrap_or(30)
+        }
+        return 60 * 30 as u64
+    };
     static ref VALS: RwLock<HashMap<String,String>> =  { 
         let mut mp = HashMap::new();
         mp.insert("FETCH_COUNT".to_string(), "0".to_string());
@@ -122,14 +153,15 @@ fn main() {
     let mut _download = std::thread::spawn(move || {
         loop {
             let download_img = download_image();
-            if !Path::new(download_img.as_str()).exists() {
-                format!("图片不存在 [{}]; 5 秒后重试 ",download_img).as_str().to_log();
+            let dip = Path::new(&download_img);
+            if !dip.exists() || !dip.is_file()  {
+                format!("下载图片不存在 [{}], 检查下载脚本; 5 秒后重试 ",download_img).as_str().to_log();
                 std::thread::sleep(Duration::from_secs(5));
                 continue;
             }
 
-            std::fs::copy(download_img.as_str(), IMG_DIR.to_string().add("/").add(C_BG)).expect("download image -> std::fs::copy erro");
             format!("图片下载完成 {} -> {}",download_img,IMG_DIR.to_string().add("/").add(C_BG)).as_str().to_log();
+            std::fs::copy(download_img.as_str(), IMG_DIR.to_string().add("/").add(C_BG)).expect("download image -> std::fs::copy erro");
             std::thread::sleep(Duration::from_secs(*DURA));
         }
     });
@@ -164,6 +196,19 @@ trait PathExt {
 
 trait StrExt {
     fn to_log(&self);
+}
+
+trait ArgExt {
+    fn get_arg_name(&mut self,name:&str)->Option<String>;
+}
+
+impl ArgExt for Args {
+    fn get_arg_name(&mut self, name: &str) -> Option<String> {
+        if let Some(val) = self.find(|v| v.starts_with(name)) {
+            return Some(val[name.len()..].to_string())
+        }
+        return None
+    }
 }
 
 impl StrExt for &str {
