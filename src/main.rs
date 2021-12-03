fd3b7cb3eb90477154f271ba441c88fca6b50710
mi78108 <mi78108@live.com>
2021-12-03 14:19:54 +0800

    rust 版本;基本能用;壁纸更换使用shell命令feh;下一步尝试直接使用x11

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,21 @@ use std::{time::{Duration, SystemTime, UNIX_EPOCH}, thread, process::{Command, E
 
 
 fn download_image() -> String {
-    eprintln!(">>> download background at {}:{}",SystemTime::now().get_current_min(),SystemTime::now().get_current_sec());
     let mut _d_img = IMG_DIR.to_string().add("/");
     if let Some(scripts) = Path::new(*FETCH_SCRIPT_DIR).get_exec_script() {
-
         let index = (|| {
             if let Ok(read) = VALS.read() {
-                return read.get("FETCH_INDEX").unwrap().parse::<usize>().unwrap()
-            }; 
-            return 0
+                return read.get("FETCH_INDEX").unwrap().parse::<usize>().unwrap();
+            };
+            return 0;
         })();
 
         if let Ok(mut write) = VALS.write() {
             write.get_mut("FETCH_INDEX").unwrap().clear();
             write.get_mut("FETCH_INDEX").unwrap().push_str((index + 1).to_string().as_str());
-        }; 
+        };
 
-        if let Some(script) = scripts.get(index % scripts.len()){
+        if let Some(script) = scripts.get(index % scripts.len()) {
             let val = (|| {
                 if let Ok(read) = VALS.read() {
                     let _val = read.get(script.file_name().to_str().unwrap()).unwrap_or("".to_string().borrow()).clone();
@@ -28,80 +26,85 @@ fn download_image() -> String {
                         read.get("FETCH_INDEX").unwrap().parse::<usize>().unwrap(),
                         read.get("FETCH_COUNT").unwrap().parse::<usize>().unwrap(),
                         Some(_val)
-                        )
-                }; 
-                return (0,0,None)
+                    );
+                };
+                return (0, 0, None);
             })();
             _d_img.push_str(script.file_name().to_string_lossy().add("_").add(val.1.to_string().as_str()).borrow());
-            println!(">>> find script {:?} [{}] [{}]",script,_d_img,val.0);
+            format!("执行下载脚本 [{:?}] [{}] [{}]",script.file_name(),_d_img,val.0).as_str().to_log();
             if let Ok(mut child) = Command::new(script.path())
                 .arg(_d_img.as_str())
-                    .arg(val.0.to_string()).spawn() 
-                    {
-                        if let Ok(code) = child.wait(){
-                            if code.success() {
-                                eprintln!(">>> download image {:?} ok at {}:{}\n\n",script.file_name(),SystemTime::now().get_current_min(),SystemTime::now().get_current_sec());
-                                return _d_img;
-                            }
-                        }
+                .arg(val.0.to_string()).spawn()
+            {
+                if let Ok(code) = child.wait() {
+                    if code.success() {
+                        format!("执行处理脚本 [{:?}] [{}] [{}] 成功; 脚本正常退出",script.file_name(),_d_img,val.0).as_str().to_log();
+                        return _d_img;
                     }
-            eprintln!(">>> download image {:?} erro at {}:{}\n\n",script.file_name(),SystemTime::now().get_current_min(),SystemTime::now().get_current_sec());
+                }
+            }
+            format!("执行下载脚本 [{:?}] [{}] [{}] 失败; 脚本异常退出",script.file_name(),_d_img,val.0).as_str().to_log();
         }
     }
     return _d_img;
 }
 
-fn process_image() -> String{
+fn process_image() -> String {
     let mut _c_img = IMG_DIR.to_string().add("/").add(C_BG);
     if let Some(scripts) = Path::new(*POST_SCRIPT_DIR).get_exec_script() {
-        scripts.iter().enumerate().for_each(|(index,fd)|{
+        scripts.iter().enumerate().for_each(|(index, fd)| {
             let _n_img = IMG_DIR.to_string().add("/").add(index.to_string().as_str()).add("_").add(Path::new(_c_img.as_str()).file_name().unwrap().to_str().unwrap());
-            println!(">>> find script {:?} [{}] [{}]",fd,_c_img,_n_img );
+            format!("执行处理脚本 [{:?}] [{}] [{}]",fd.file_name(),_c_img,_n_img).as_str().to_log();
             if let Ok(mut child) = Command::new(fd.path())
                 .arg(_c_img.as_str())
-                    .arg(_n_img.as_str()).spawn() 
-                    {
-                        if let Ok(code) = child.wait(){
-                            if code.success() {
-                                _c_img.clear();
-                                if Path::new(_n_img.as_str()).exists(){
-                                    _c_img.push_str(_n_img.as_str());
-                                    eprintln!(">>> process background {:?} ok at {}:{}\n\n",fd.file_name(),SystemTime::now().get_current_min(),SystemTime::now().get_current_sec());
-                                }else {
-                                    _c_img.push_str(IMG_DIR.to_string().add("/").add(C_BG).as_str());
-                                }
-                                return;
-                            }
+                .arg(_n_img.as_str()).spawn()
+            {
+                if let Ok(code) = child.wait() {
+                    if code.success() {
+                        _c_img.clear();
+                        if Path::new(_n_img.as_str()).exists() {
+                            _c_img.push_str(_n_img.as_str());
+                            format!("执行处理脚本 [{:?}] [{}] [{}] 成功; 脚本正常退出; 输出文件存在",fd.file_name(),_c_img,_n_img).as_str().to_log();
+                        } else {
+                            _c_img.push_str(IMG_DIR.to_string().add("/").add(C_BG).as_str());
+                            format!("执行处理脚本 [{:?}] [{}] [{}] 失败; 脚本正常退出; 输出文件不在",fd.file_name(),_c_img,_n_img).as_str().to_log();
                         }
+                        return;
                     }
-            eprintln!(">>> process background {:?} erro at {}:{}\n\n",fd.file_name(),SystemTime::now().get_current_min(),SystemTime::now().get_current_sec());
+                }
+            }
+            format!("执行处理脚本 [{:?}] [{}] [{}] 失败; 脚本异常退出",fd.file_name(),_c_img,_n_img).as_str().to_log();
         });
     }
-    return _c_img
+    return _c_img;
 }
 
-fn set_background(img_path:String){
-    if let Ok(child)= Command::new("feh").arg("--bg-scale").arg(img_path).spawn(){
-        eprintln!(">>> set background at {}:{} ok\n\n",SystemTime::now().get_current_min(),SystemTime::now().get_current_sec());
-    }else{
-        eprintln!(">>> set background at {}:{} erro\n\n",SystemTime::now().get_current_min(),SystemTime::now().get_current_sec());
+fn set_background(img_path: String) {
+    if let Ok(mut child) = Command::new("feh").arg("--bg-scale").arg(img_path.as_str()).spawn() {
+        if let Ok(state) = child.wait() {
+            if state.success(){
+                format!("设置壁纸 [{}] 成功",img_path).as_str().to_log();
+                return;
+            }
+        }
     }
+    format!("设置壁纸 [{}] 失败",img_path).as_str().to_log();
 }
 
-fn change_background(){
-    eprintln!(">>> change background at {}:{}\n\n",SystemTime::now().get_current_min(),SystemTime::now().get_current_sec());
-    set_background(IMG_DIR.to_string().add("/").add(C_BG));
+fn change_background() {
+    set_background(IMG_DIR.to_string().add("/").add(N_BG));
     // process next image 
     thread::spawn(|| {
         let post_img = process_image();
-        std::fs::copy(post_img, IMG_DIR.to_string().add("/").add(N_BG)).expect("change image -> std::fs::copy erro")
+        std::fs::copy(post_img.as_str(), IMG_DIR.to_string().add("/").add(N_BG)).expect("change image -> std::fs::copy erro");
+        format!("图片处理完成 {} -> {}",post_img,IMG_DIR.to_string().add("/").add(N_BG)).as_str().to_log();
     });
 }
 
-static C_BG:&str = "bgwall_next";
-static N_BG:&str = "bgwall_current";
+static C_BG: &str = "bgwall_next";
+static N_BG: &str = "bgwall_current";
 
-lazy_static::lazy_static!{
+lazy_static::lazy_static! {
     static ref IMG_DIR:&'static str = "/tmp/hawk";
     static ref FETCH_SCRIPT_DIR:&'static str = "/home/hawk/Sync/bin/bgwall/fetch_script";
     static ref POST_SCRIPT_DIR:&'static str = "/home/hawk/Sync/bin/bgwall/post_script";
@@ -116,33 +119,37 @@ lazy_static::lazy_static!{
 
 
 fn main() {
-    let mut _download = std::thread::spawn(move ||{
+    let mut _download = std::thread::spawn(move || {
         loop {
             let download_img = download_image();
-            if  !Path::new(download_img.as_str()).exists(){
+            if !Path::new(download_img.as_str()).exists() {
+                format!("图片不存在 [{}]; 5 秒后重试 ",download_img).as_str().to_log();
+                std::thread::sleep(Duration::from_secs(5));
                 continue;
             }
-            ">>> 图片下载完成 {}".to_log();
-            std::fs::copy(download_img, IMG_DIR.to_string().add("/").add(C_BG)).expect("download image -> std::fs::copy erro");
+
+            std::fs::copy(download_img.as_str(), IMG_DIR.to_string().add("/").add(C_BG)).expect("download image -> std::fs::copy erro");
+            format!("图片下载完成 {} -> {}",download_img,IMG_DIR.to_string().add("/").add(C_BG)).as_str().to_log();
             std::thread::sleep(Duration::from_secs(*DURA));
         }
     });
 
-    let mut _change = std::thread::spawn(||{
+    let mut _change = std::thread::spawn(|| {
         loop {
-            if Path::new(IMG_DIR.to_string().add("/").add(C_BG).as_str()).exists() { 
+            if Path::new(IMG_DIR.to_string().add("/").add(C_BG).as_str()).exists() {
                 change_background();
-            }else{
+            } else {
                 let download_img = download_image();
                 set_background(download_img)
             }
-            let _sec =  SystemTime::now().get_current_sec() as u64;
+            let _sec = SystemTime::now().get_current_sec() as u64;
+            format!("图片更新完成 [{}]",IMG_DIR.to_string().add("/").add(C_BG)).as_str().to_log();
             std::thread::sleep(Duration::from_secs(60 - _sec));
         }
     });
 
     loop {
-
+        std::thread::sleep(Duration::from_secs(60 ));
     }
 }
 
@@ -162,43 +169,43 @@ trait StrExt {
 impl StrExt for &str {
     fn to_log(&self) {
         let n = SystemTime::now();
-        eprintln!(">>> [{}:{}] > {}",n.get_current_min(),n.get_current_sec(),self);
-    }   
+        eprintln!("<{}:{}> [LOG]: {}", n.get_current_min(), n.get_current_sec(), self);
+    }
 }
 
 impl PathExt for Path {
     fn get_exec_script(&self) -> Option<Vec<DirEntry>> {
         if let Ok(dir) = self.read_dir() {
-            let mut scripts = dir.filter(|fs|{
+            let mut scripts = dir.filter(|fs| {
                 if let Ok(sct) = fs {
                     if sct.path().is_file() {
-                        if let Ok(md) = sct.metadata(){
-                            if ( md.mode() & 0o111 ) != 0 {
-                                return true
+                        if let Ok(md) = sct.metadata() {
+                            if (md.mode() & 0o111) != 0 {
+                                return true;
                             }
                         }
                     }
                 }
-                return false
+                return false;
             }).map(|v| v.unwrap()).collect::<Vec<DirEntry>>();
 
-            scripts.sort_by(|v1,v2|{
-                return v1.file_name().cmp(&v2.file_name())
+            scripts.sort_by(|v1, v2| {
+                return v1.file_name().cmp(&v2.file_name());
             });
             return Some(scripts);
         }
-        return None
+        return None;
     }
 }
 
 impl TimeExt for SystemTime {
     fn get_current_sec(&self) -> u8 {
         let total_sec = self.duration_since(UNIX_EPOCH).unwrap().as_secs();
-        return  ( total_sec % ( 60 * 24 ) % 60 ) as u8;
+        return (total_sec % (60 * 24) % 60) as u8;
     }
 
-    fn get_current_min(&self) -> u8{
+    fn get_current_min(&self) -> u8 {
         let total_sec = self.duration_since(UNIX_EPOCH).unwrap().as_secs();
-        return  ( total_sec % ( 60 * 24 ) % ( 60 * 60 ) / 60 ) as u8;
+        return (total_sec % (60 * 24) % (60 * 60) / 60) as u8;
     }
 }
